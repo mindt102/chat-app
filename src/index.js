d59aa56d0732d6eaba8d156ba5d24193c4f662fd
minhdt102 <minhdt24082002@gmail.com>
2021-09-01 16:34:27 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const {
+    generateMessage,
+    generateLocationMessage,
+} = require("./utils/messages");
+const {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket connection");
+
+    socket.on("join", (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options });
+
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+
+        socket.join(user.room);
+
+        socket.emit("message", generateMessage("Admin", "Welcome!"));
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                "message",
+                generateMessage("Admin", `${user.username} has joined!`)
+            ); // To everyone else except the current socket
+
+        io.to(user.room).emit("roomData", {
+            room: user.room,
+            users: getUsersInRoom(user.room),
+        });
+        callback();
+    });
+
+    socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id);
+
+        if (!user) {
+            return callback("Cannot find user");
+        }
+
+        const filter = new Filter();
+
+        if (filter.isProfane(message)) {
+            return callback("Profanity is not allowed");
+        }
+
+        io.to(user.room).emit(
+            "message",
+            generateMessage(user.username, message)
+        );
+        callback();
+    });
+
+    socket.on("shareLocation", (coords, callback) => {
+        const user = getUser(socket.id);
+
+        if (!user) {
+            return callback("Cannot find user");
+        }
+        io.to(user.room).emit(
+            "locationMessage",
+            generateLocationMessage({ username: user.username, ...coords })
+        );
+        callback();
+    });
+
+    socket.on("disconnect", () => {
+        const user = removeUser(socket.id);
+        if (user) {
+            io.to(user.room).emit(
+                "message",
+                generateMessage("Admin", `${user.username} has left`)
+            );
+            io.to(user.room).emit("roomData", {
+                room: user.room,
+                users: getUsersInRoom(user.room),
+            });
+        }
+    });
+
+    // socket.emit("countUpdated", count); Only send to one client
+    // io.emit("countUpdated", count); // Send to all clients
+});
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+});
